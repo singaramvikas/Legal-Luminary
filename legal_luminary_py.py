e9e67e866c06606e0781b3a51825134a520cbd00
singaramvikas <114528198+singaramvikas@users.noreply.github.com>
2024-07-25 15:53:26 -0500

    Update legal_luminary_py.py

--- legal_luminary_py.py
+++ legal_luminary_py.py
@@ -78,7 +78,7 @@ with st.sidebar:
     st.markdown(
         """
         <div class="sidebar-title">
-            <img src="https://raw.githubusercontent.com/Vikas-Singaram/ShockerBot/3a323888507453b5639e7168f79c16cf9ed2ba39/wsu_logo.png" width="30" />
+            <img src="https://raw.githubusercontent.com/singaramvikas/Legal-Luminary/blob/main/legal%20luminary%20logo.jpg" width="30" />
             <h1>Singarams Leagal Advisor</h1>
         </div>
         """,
